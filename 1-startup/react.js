a12ddbe5af1d0893d0d72985b7502b9fffbdff6b
iffishells <iffishells@gmail.com>
2020-08-13 22:16:47 +0500

    Hellow world

--- 1-startup/react.js
+++
@@ -1,6 +0,0 @@
-console.log("test")
-if (2+20==22) {
-  console.log("True");
-} else {
-  console.log("False")
-}
--- 5-Destructering/array_function.js
+++
@@ -1,8 +0,0 @@
-console.log("connected");
-
-const numbers  = [1 , 2 , 3];
-console.log(numbers);
-const doublearray = numbers.map((num) => {
-  return 2 * num;
-});
-console.log(doublearray);
