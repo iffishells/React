aa8804795d7c12b702c23482a8d00a519478e0cf
iffishells <iffishells@gmail.com>
2020-10-26 00:00:05 +0500

    starting Now from zero to here

---
+++ started/Hellow-world/my-app/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      fname : "Muhammad",
+      lname : "iftikhar",
+      dob : "1998/9/3"
+    }
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <p><h1>{ this.state.fname } </h1>
+          <h1>{this.state.lname} </h1>
+          <h1> {this.state.dob} </h1>
+          <h1>now updated </h1> </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+export default App;
